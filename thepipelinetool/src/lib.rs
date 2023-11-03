1f0a4dc446d894a346668f5fdc16e3e144c9b7b0
thepipelinetool <149430189+thepipelinetool@users.noreply.github.com>
2023-11-03 03:39:50 +0000

    use options

--- thepipelinetool/src/lib.rs
+++ thepipelinetool/src/lib.rs
@@ -5,6 +5,7 @@ mod options;
 pub mod prelude {
     pub use crate::dag::DAG;
     pub use crate::cli::*;
+    pub use crate::options::*;
     pub use runner::local::{LocalRunner, hash_dag};
     pub use runner::{DefRunner, Runner};
     pub use serde_json::{json, Value};
