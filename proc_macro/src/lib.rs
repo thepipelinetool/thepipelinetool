c8ae2e82ed2b93dcd2d897767d58020f901eff71
Derrick Liu <derrick56007@hotmail.com>
2023-11-19 03:13:50 -0800

    fmt

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -1,17 +1,3 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
