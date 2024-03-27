fd4c16ad612c88743e829a4044f905e1af309e15
Derrick <derrick56007@github.com>
2024-03-26 18:00:28 -0700

    refactor

--- thepipelinetool/src/cli.rs
+++ thepipelinetool/src/cli.rs
@@ -14,6 +14,173 @@ use thepipelinetool_utils::execute_function_using_json_files;
 use crate::hash::hash_dag;
 use crate::*;
 
+fn describe() {
+    let tasks: std::sync::RwLockReadGuard<'_, Vec<Task>> = get_tasks().read().unwrap();
+    let functions = get_functions().read().unwrap();
+
+    println!("Task count: {}", tasks.len());
+    println!(
+        "Functions: {:#?}",
+        functions.keys().collect::<Vec<&String>>()
+    );
+}
+
+fn display_tasks() {
+    let tasks = get_tasks().read().unwrap();
+
+    println!("{}", serde_json::to_string_pretty(&*tasks).unwrap());
+}
+
+fn display_edges() {
+    let edges = get_edges().read().unwrap();
+
+    println!("{}", serde_json::to_string_pretty(&*edges).unwrap());
+}
+
+fn display_hash() {
+    let tasks = get_tasks().read().unwrap();
+    let edges = get_edges().read().unwrap();
+
+    let hash = hash_dag(
+        &serde_json::to_string(&*tasks).unwrap(),
+        &edges.iter().copied().collect::<Vec<(usize, usize)>>(),
+    );
+    print!("{hash}");
+}
+
+fn display_graph(graph_type: &str) {
+    let tasks = get_tasks().read().unwrap();
+    let edges = get_edges().read().unwrap();
+
+    let mut runner = InMemoryRunner::new(&tasks, &edges);
+    runner.enqueue_run("in_memory", "", Utc::now());
+
+    match graph_type {
+        "mermaid" => {
+            let graph = runner.get_mermaid_graph(0);
+            print!("{graph}");
+        }
+        "graphite" => {
+            let graph = runner.get_graphite_graph(0);
+            print!("{}", serde_json::to_string_pretty(&graph).unwrap());
+        }
+        o => {
+            panic!("undefined graph type: {o}");
+        }
+    }
+}
+
+fn display_tree() {
+    let tasks = get_tasks().read().unwrap();
+    let edges = get_edges().read().unwrap();
+
+    let mut runner = InMemoryRunner::new(&tasks, &edges);
+    let run_id = runner.enqueue_run("in_memory", "", Utc::now());
+    let tasks = runner
+        .get_default_tasks()
+        .iter()
+        .filter(|t| runner.get_task_depth(run_id, t.id) == 0)
+        .map(|t| t.id)
+        .collect::<Vec<usize>>();
+
+    let mut output = "DAG\n".to_string();
+    let mut task_ids_in_order: Vec<usize> = vec![];
+
+    for (index, child) in tasks.iter().enumerate() {
+        let is_last = index == tasks.len() - 1;
+
+        let connector = if is_last { "└── " } else { "├── " };
+        task_ids_in_order.push(*child);
+        output.push_str(&runner.get_tree(
+            run_id,
+            *child,
+            1,
+            connector,
+            vec![is_last],
+            &mut task_ids_in_order,
+        ));
+    }
+    println!("{}", output);
+    // println!("{:?}", task_ids_in_order);
+}
+
+pub fn run_in_memory(num_threads: usize) {
+    dbg!(1);
+    let tasks = get_tasks().read().unwrap();
+    let edges = get_edges().read().unwrap();
+
+    let mut runner = InMemoryRunner::new(&tasks.to_vec(), &edges);
+
+    let run_id = runner.enqueue_run("", "", Utc::now());
+
+    let default_tasks = runner.get_default_tasks();
+    dbg!(2);
+    // check for circular dependencies
+    for task in &default_tasks {
+        let mut visited = HashSet::new();
+        let mut path = vec![];
+        let circular_dependencies =
+            runner.get_circular_dependencies(run_id, task.id, &mut visited, &mut path);
+
+        if let Some(deps) = circular_dependencies {
+            panic!("{:?}", deps);
+        }
+    }
+
+    let (tx, rx) = channel();
+
+    let mut thread_count = 0;
+
+    for _ in 0..num_threads {
+        let mut runner = runner.clone();
+        let tx = tx.clone();
+        dbg!(3);
+        if let Some(queued_task) = runner.pop_priority_queue() {
+            thread::spawn(move || {
+                let current_executable_path = &env::args().next().unwrap();
+
+                runner.work(run_id, &queued_task, current_executable_path.as_str());
+                tx.send(()).unwrap();
+            });
+
+            thread_count += 1;
+            if thread_count >= num_threads {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    for _ in rx.iter() {
+        dbg!(4);
+        thread_count -= 1;
+
+        let mut runner = runner.clone();
+        if let Some(queued_task) = runner.pop_priority_queue() {
+            let tx = tx.clone();
+
+            thread::spawn(move || {
+                let current_executable_path = &env::args().next().unwrap();
+
+                runner.work(run_id, &queued_task, current_executable_path.as_str());
+                tx.send(()).unwrap();
+            });
+
+            thread_count += 1;
+
+            if thread_count >= num_threads {
+                continue;
+            }
+        }
+        if thread_count == 0 {
+            drop(tx);
+            break;
+        }
+    }
+    dbg!(5);
+}
+
 /// Parses command-line arguments and executes various tasks in the DAG CLI tool.
 ///
 /// This function parses command-line arguments using the `command!` macro and executes
@@ -116,122 +283,22 @@ pub fn parse_cli() {
 
     if let Some(subcommand) = matches.subcommand_name() {
         match subcommand {
-            "describe" => {
-                let tasks: std::sync::RwLockReadGuard<'_, Vec<Task>> = get_tasks().read().unwrap();
-                let functions = get_functions().read().unwrap();
-
-                println!("Task count: {}", tasks.len());
-                println!(
-                    "Functions: {:#?}",
-                    functions.keys().collect::<Vec<&String>>()
-                );
-            }
-            "tasks" => {
-                let tasks = get_tasks().read().unwrap();
-
-                println!("{}", serde_json::to_string_pretty(&*tasks).unwrap());
-            }
-            "edges" => {
-                let edges = get_edges().read().unwrap();
-
-                println!("{}", serde_json::to_string_pretty(&*edges).unwrap());
-            }
+            "describe" => describe(),
+            "tasks" => display_tasks(),
+            "edges" => display_edges(),
             "graph" => {
                 let matches = matches.subcommand_matches("graph").unwrap();
                 if let Some(subcommand) = matches.get_one::<String>("graph_type") {
-                    let tasks = get_tasks().read().unwrap();
-                    let edges = get_edges().read().unwrap();
-
-                    let mut runner = InMemoryRunner::new(&tasks, &edges);
-                    runner.enqueue_run("in_memory", "", Utc::now());
-
-                    match subcommand.as_str() {
-                        "mermaid" => {
-                            let graph = runner.get_mermaid_graph(0);
-                            print!("{graph}");
-                        }
-                        "graphite" => {
-                            let graph = runner.get_graphite_graph(0);
-                            print!("{}", serde_json::to_string_pretty(&graph).unwrap());
-                        }
-                        o => {
-                            panic!("undefined graph type: {o}");
-                        }
-                    }
+                    display_graph(subcommand.as_str());
                 }
             }
-            "hash" => {
-                let tasks = get_tasks().read().unwrap();
-                let edges = get_edges().read().unwrap();
-
-                let hash = hash_dag(
-                    &serde_json::to_string(&*tasks).unwrap(),
-                    &edges.iter().copied().collect::<Vec<(usize, usize)>>(),
-                );
-                print!("{hash}");
-            }
-            "tree" => {
-                let tasks = get_tasks().read().unwrap();
-                let edges = get_edges().read().unwrap();
-
-                let mut runner = InMemoryRunner::new(&tasks, &edges);
-                let run_id = runner.enqueue_run("in_memory", "", Utc::now());
-                let tasks = runner
-                    .get_default_tasks()
-                    .iter()
-                    .filter(|t| runner.get_task_depth(run_id, t.id) == 0)
-                    .map(|t| t.id)
-                    .collect::<Vec<usize>>();
-
-                let mut output = "DAG\n".to_string();
-                let mut task_ids_in_order: Vec<usize> = vec![];
-
-                for (index, child) in tasks.iter().enumerate() {
-                    let is_last = index == tasks.len() - 1;
-
-                    let connector = if is_last { "└── " } else { "├── " };
-                    task_ids_in_order.push(*child);
-                    output.push_str(&runner.get_tree(
-                        run_id,
-                        *child,
-                        1,
-                        connector,
-                        vec![is_last],
-                        &mut task_ids_in_order,
-                    ));
-                }
-                println!("{}", output);
-                // println!("{:?}", task_ids_in_order);
-            }
+            "hash" => display_hash(),
+            "tree" => display_tree(),
             "run" => {
                 let matches = matches.subcommand_matches("run").unwrap();
                 if let Some(subcommand) = matches.subcommand_name() {
                     match subcommand {
                         "in_memory" => {
-                            let tasks = get_tasks().read().unwrap();
-                            let edges = get_edges().read().unwrap();
-
-                            let mut runner = InMemoryRunner::new(&tasks.to_vec(), &edges);
-
-                            let run_id = runner.enqueue_run("", "", Utc::now());
-
-                            let default_tasks = runner.get_default_tasks();
-
-                            for task in &default_tasks {
-                                let mut visited = HashSet::new();
-                                let mut path = vec![];
-                                let circular_dependencies = runner.get_circular_dependencies(
-                                    run_id,
-                                    task.id,
-                                    &mut visited,
-                                    &mut path,
-                                );
-
-                                if let Some(deps) = circular_dependencies {
-                                    panic!("{:?}", deps);
-                                }
-                            }
-
                             let num_threads = match matches
                                 .subcommand_matches("in_memory")
                                 .unwrap()
@@ -245,65 +312,7 @@ pub fn parse_cli() {
                                 ),
                                 any => any.parse::<usize>().unwrap(),
                             };
-
-                            let (tx, rx) = channel();
-
-                            let mut thread_count = 0;
-
-                            for _ in 0..num_threads {
-                                let mut runner = runner.clone();
-                                let tx = tx.clone();
-
-                                if let Some(queued_task) = runner.pop_priority_queue() {
-                                    thread::spawn(move || {
-                                        let current_executable_path = &env::args().next().unwrap();
-
-                                        runner.work(
-                                            run_id,
-                                            &queued_task,
-                                            current_executable_path.as_str(),
-                                        );
-                                        tx.send(()).unwrap();
-                                    });
-
-                                    thread_count += 1;
-                                    if thread_count >= num_threads {
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-
-                            for _ in rx.iter() {
-                                thread_count -= 1;
-
-                                let mut runner = runner.clone();
-                                if let Some(queued_task) = runner.pop_priority_queue() {
-                                    let tx = tx.clone();
-
-                                    thread::spawn(move || {
-                                        let current_executable_path = &env::args().next().unwrap();
-
-                                        runner.work(
-                                            run_id,
-                                            &queued_task,
-                                            current_executable_path.as_str(),
-                                        );
-                                        tx.send(()).unwrap();
-                                    });
-
-                                    thread_count += 1;
-
-                                    if thread_count >= num_threads {
-                                        continue;
-                                    }
-                                }
-                                if thread_count == 0 {
-                                    drop(tx);
-                                    break;
-                                }
-                            }
+                            run_in_memory(num_threads);
 
                             // let tasks = runner
                             //     .get_default_tasks()
