5b0f833abc06d0dac57d7a586ac31994de17ab04
Derrick <derrick56007@github.com>
2024-04-09 15:45:51 -0700

    test parallelism on build

--- scripts/run_all_examples.sh
+++ scripts/run_all_examples.sh
@@ -6,17 +6,17 @@ do
     echo "running example: $f"
 
     if [[ "$f" == "params"* ]]; then
-        tpt ./bin/$f run in_memory --params '{"data": "hello world"}'
+        tpt ./bin/$f run in_memory --params '{"data": "hello world"}' --max_parallelism $1
         retVal=$?
     elif [[ "$f" == "fail"* ]]; then
-        tpt ./bin/$f run in_memory
+        tpt ./bin/$f run in_memory --max_parallelism $1
         retVal=$?
         if [ $retVal -eq 0 ]; then
             echo "error: $f should have failed"
             exit 1
         fi
     else
-        tpt ./bin/$f run in_memory
+        tpt ./bin/$f run in_memory --max_parallelism $1
         retVal=$?
         if [ $retVal -ne 0 ]; then
             echo "error: $f failed"
