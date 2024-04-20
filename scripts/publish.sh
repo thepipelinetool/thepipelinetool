dbc59edafb674db6df32b55b04991cd5344265a5
dirk <dirk@rexs-air.lan>
2024-04-19 23:38:06 -0700

    reorder

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 cargo publish -p thepipelinetool_utils
-cargo publish -p thepipelinetool_runner
 cargo publish -p thepipelinetool_task
+cargo publish -p thepipelinetool_runner
 cargo publish -p thepipelinetool_operators
 cargo publish -p thepipelinetool_core
 cargo publish -p thepipelinetool
