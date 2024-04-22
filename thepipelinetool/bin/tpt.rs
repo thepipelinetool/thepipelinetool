da693fe36df311f00b34254d760ffe3645025edd
dirk <dirk@rexs-air.lan>
2024-04-21 19:30:58 -0700

    allow running of non-executable pipeline definitions without file upload

--- thepipelinetool/bin/tpt.rs
+++ thepipelinetool/bin/tpt.rs
@@ -10,16 +10,16 @@ use thepipelinetool::{
     commands::create_commands, process_subcommands, read_from_executable::read_from_executable,
     read_from_yaml::read_from_yaml, source_type::SourceType,
 };
-use thepipelinetool_core::dev::*;
+use thepipelinetool_core::dev::{
+    assert::assert_operator, params::params_operator, print::print_operator, *,
+};
 use thepipelinetool_runner::pipeline_options::PipelineOptions;
 
 fn main() -> Result<()> {
     let mut args: Vec<String> = env::args().collect();
-    let command = create_commands().arg(Arg::new("pipeline_source").required(true));
+    let command = create_commands().arg(Arg::new("pipeline_source"));
     let matches = command.get_matches();
-    let pipeline_source = matches
-        .get_one::<String>("pipeline_source")
-        .expect("required");
+    let pipeline_source = matches.get_one::<String>("pipeline_source");
 
     let source_type = SourceType::from_source(pipeline_source);
 
@@ -28,7 +28,7 @@ fn main() -> Result<()> {
     match source_type {
         SourceType::Exe => {
             if args.len() > 4 && args[2..4] == ["run", "function"] {
-                let mut cmd = Command::new(pipeline_source);
+                let mut cmd = Command::new(pipeline_source.unwrap());
                 cmd.args(&mut args[2..]);
                 let exit_status = spawn(
                     cmd,
@@ -44,27 +44,49 @@ fn main() -> Result<()> {
                 )?;
                 process::exit(exit_status.code().unwrap());
             } else {
-                read_from_executable(pipeline_source)
+                read_from_executable(pipeline_source.unwrap())
             }
         }
         SourceType::Yaml => {
-            read_from_yaml(serde_yaml::from_reader(File::open(pipeline_source)?)?);
+            read_from_yaml(serde_yaml::from_reader(File::open(
+                pipeline_source.unwrap(),
+            )?)?);
         }
         SourceType::Raw => {
-            read_from_yaml(serde_json::from_str(pipeline_source)?);
+            read_from_yaml(serde_json::from_str(pipeline_source.unwrap())?);
+        }
+        SourceType::None => {
+            for built_in_operator in vec![
+                Operator::BashOperator,
+                Operator::ParamsOperator,
+                Operator::PrintOperator,
+                Operator::AssertOperator,
+            ] {
+                _register_function_with_name(
+                    match built_in_operator {
+                        Operator::BashOperator => bash_operator,
+                        Operator::ParamsOperator => params_operator,
+                        Operator::PrintOperator => print_operator,
+                        Operator::AssertOperator => assert_operator,
+                    },
+                    &json!(built_in_operator).as_str().unwrap(),
+                );
+            }
         }
     }
 
     let options = match source_type {
         SourceType::Exe => PipelineOptions::default(), // TODO read options from exe?
-        SourceType::Yaml => serde_yaml::from_reader(File::open(pipeline_source)?)?,
-        SourceType::Raw => serde_yaml::from_str(pipeline_source)?,
+        SourceType::Yaml => serde_yaml::from_reader(File::open(pipeline_source.unwrap())?)?,
+        SourceType::Raw => serde_yaml::from_str(pipeline_source.unwrap())?,
+        SourceType::None => PipelineOptions::default(),
     };
 
     let pipeline_path = match source_type {
-        SourceType::Exe => pipeline_source,
-        SourceType::Yaml => pipeline_source,
-        SourceType::Raw => &args[0],
+        SourceType::Exe => pipeline_source.unwrap(),
+        SourceType::Yaml => "",
+        SourceType::Raw => "",
+        SourceType::None => "",
     };
 
     process_subcommands(pipeline_path, subcommand_name, &options, &matches)?;
--- thepipelinetool/src/source_type.rs
+++ thepipelinetool/src/source_type.rs
@@ -5,21 +5,31 @@ pub enum SourceType {
     Exe,
     Yaml,
     Raw,
+    None,
 }
 
 impl SourceType {
-    pub fn from_source(source: &str) -> Self {
-        let p = Path::new(source);
-        if p.exists() {
-            match p.extension() {
-                Some(ext) => match ext.to_str().unwrap() {
-                    "yaml" => SourceType::Yaml,
-                    _ => panic!("unknown extenstion type"),
-                },
-                None => SourceType::Exe,
+    pub fn from_source(source: Option<&String>) -> Self {
+        if let Some(source) = source {
+            if source == "" {
+                SourceType::None
+
+            } else {
+                let p = Path::new(source);
+                if p.exists() {
+                    match p.extension() {
+                        Some(ext) => match ext.to_str().unwrap() {
+                            "yaml" => SourceType::Yaml,
+                            _ => panic!("unknown extenstion type"),
+                        },
+                        None => SourceType::Exe,
+                    }
+                } else {
+                    SourceType::Raw
+                }
             }
         } else {
-            SourceType::Raw
+            SourceType::None
         }
     }
 }
